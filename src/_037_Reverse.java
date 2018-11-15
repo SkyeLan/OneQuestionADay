87abd64801d04db7fff9e2ff017005cf201513ed
Skye <lantian9494@163.com>
2018-11-15 10:29:22 +0800

    反转一个3位整数

---
+++ src/_037_Reverse.java
@@ -0,0 +1,6 @@
+public class _037_Reverse {
+    public static void main(String[] args) {
+        int number = 123;
+        System.out.println(number / 100 + number % 100 + number % 10 * 99);
+    }
+}
